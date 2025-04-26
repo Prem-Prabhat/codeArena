595a7fa6977a9b9b8991f62789e27d084d2ce3fc
Prem-Prabhat <premprabhat960838@gmail.com>
2025-04-27 03:59:01 +0530

    feat(auth): Implement user login functionality with JWT

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -56,11 +56,68 @@ export const register = async (req, res) => {
     }
 }
 
-export const login = async (req, res) => { }
+export const login = async (req, res) => {
+    const { email, password } = req.body
 
-export const logout = async (req, res) => { }
+    try {
+        const user = await db.user.findUnique({
+            where: {
+                email
+            }
+        })
 
-export const check = async (req, res) => { }
+        if (!user) {
+            return res.status(400).json({
+                error: "User not found"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            return res.status(400).json({
+                error: "Invalid credentials"
+            })
+        }
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+            expiresIn: "1d"
+        })
+
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== "development",
+            sameSite: "strict",
+            maxAge: 24 * 60 * 60 * 1000 // 1 day
+        })
+
+        res.status(200).json({
+            success: true,
+            message: "User logged in successfully",
+            user: {
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                role:user.role,
+                image:user.image
+            }
+        })
+    } catch (error) {
+        console.error("Error logging in:",error)
+        res.status(500).json({
+            error: "Internal server error"
+        })
+    }
+}
+
+export const logout = async (req, res) => {
+   
+   
+}
+
+export const check = async (req, res) => {
+    
+}
 
 export const forgotPassword = async (req, res) => { }
 
